56858075e8b0ed69d894ed5756d38e57de393337
garrathjones <jones.garrath@gmail.com>
2022-01-28 23:02:03 +0000

    Organised project files, created prefabs, added a Movement script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("thrusting");
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("right");
+        }
+    }
+
+
+}
